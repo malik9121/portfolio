7432dc9a7268a6a2aa09c066478672d682c08e4d
Muhammad Ehsan <m.ehsan.contact@gmail.com>
2026-02-23 01:50:18 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { Menu, X, Mail, Linkedin, Github, Instagram, Youtube, Download, ChevronDown, Send } from 'lucide-react';
+import { Menu, X, Mail, Linkedin, Github, Instagram, Youtube, Download, ChevronDown, Send, Music } from 'lucide-react';
 
 import { AnimatedBorderCard } from '@/components/AnimatedBorderCard';
 
